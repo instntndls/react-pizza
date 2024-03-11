60779ab78d2e2c1b307f59993f721513438143ea
instntndls <farn7229@gmail.com>
2024-03-11 12:57:02 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+function PizzaRender(amount) {
+    const arr = [];
+    for (let i = 0; i < amount; i++) {
+        arr.push(<PizzaBlock/>)
+    }
+    return arr
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+        <Header/>
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories/>
+              <Sort/>
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {PizzaRender(10)}
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
